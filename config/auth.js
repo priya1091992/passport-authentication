ca7a588842899625eb444ab7661740f2ef2d6196
Priya Choudhary <priya1091992@gmail.com>
2016-12-26 00:03:14 +0530

    local-authentication

---
+++ config/auth.js
@@ -0,0 +1,3 @@
+/**
+ * Created by priya on 25/12/16.
+ */
---
+++ config/database.js
@@ -0,0 +1,8 @@
+/**
+ * Created by priya on 25/12/16.
+ */
+module.exports = {
+
+    'url' : 'mongodb://127.0.0.1:27017/UserPassportAuth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
